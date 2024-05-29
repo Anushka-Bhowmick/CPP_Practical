298028f5081829f9249b0d7b640f324cf646da02
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2024-05-29 22:00:00 +0530

    modified A5

--- A5.cpp
+++ A5.cpp
@@ -1,49 +1,74 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-class Height {
-private:
-    int feet;
-    int inches;
-
-public:
-    Height(int ft = 0, int in = 0) : feet(ft), inches(in) {}
-
-    double into_cm() const {
-        return feet * 30.48 + inches * 2.54;
-    }
-
-    Height operator-(const Height& other) const {
-        int totalInches1 = feet * 12 + inches;
-        int totalInches2 = other.feet * 12 + other.inches;
-        int diff = totalInches1 - totalInches2;
-        int ft = diff / 12;
-        int in = diff % 12;
-        return Height(ft, in);
-    }
-
-    bool operator<(const Height& other) const {
-        int totalInches1 = feet * 12 + inches;
-        int totalInches2 = other.feet * 12 + other.inches;
-        return totalInches1 < totalInches2;
-    }
-
-    bool operator==(const Height& other) const {
-        return feet == other.feet && inches == other.inches;
-    }
+class HEIGHT{
+    private:
+        int feet;
+        int inches;
+    public:
+        HEIGHT(int f=0, int i=0){
+            feet = f;
+            inches = i;
+        }
+        double into_cm(){
+            return (feet*30.48) + (inches*2.54);
+        }
+        HEIGHT operator-(const HEIGHT &h){
+            int f = feet - h.feet;
+            int i = inches - h.inches;
+            if(i < 0){
+                f--;
+                i += 12;
+            }
+            return HEIGHT(f, i);
+        }
+        bool operator>(const HEIGHT &h){
+            if(feet > h.feet){
+                return true;
+            }
+            else if(feet == h.feet && inches > h.inches){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        bool operator<(const HEIGHT &h){
+            if(feet < h.feet){
+                return true;
+            }
+            else if(feet == h.feet && inches < h.inches){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        void display(){
+            cout<<"Feet: "<<feet<<" Inches: "<<inches<<endl;
+        }
 };
-
-int main() {
-    Height h1(5, 9), h2(6, 2);
-    Height diff = h1 - h2;
-    cout << "Difference of heights: " << diff.into_cm() << " cm" << endl;
-
-    if (h1 < h2)
-        cout << "Height 1 is less than Height 2" << endl;
-    else if (h1 == h2)
-        cout << "Height 1 is equal to Height 2" << endl;
-    else
-        cout << "Height 1 is greater than Height 2" << endl;
-
+int main(){
+    int f1, f2, i1, i2;
+    cout<<"Enter the height of the first person in feet and inches: ";
+    cin>>f1>>i1;
+    cout<<"Enter the height of the second person in feet and inches: ";
+    cin>>f2>>i2;
+    HEIGHT h1(f1, i1), h2(f2, i2), h3;
+    h3 = h1 - h2;
+    cout<<"The difference of the two heights is: "<<    h3.into_cm();
+    cout<<" cm"<<endl;
+    h3.display();
+    if(h1 > h2){
+        cout<<"The first person is taller than the second person"<<endl;
+    }
+    else if(h1 < h2){
+        cout<<"The first person is shorter than the second person"<<endl;
+    }
+    else{
+        cout<<"Both the persons are of the same height"<<endl;
+    }
+    cout<<"The height of the first person in cm is: "<<h1.into_cm()<<endl;
+    cout<<"The height of the second person in cm is: "<<h2.into_cm()<<endl;
     return 0;
+
 }

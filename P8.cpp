82a7844a9387f83e0ba21d3f386cde5b40b00da2
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2024-05-29 21:38:59 +0530

    modify

--- P8.cpp
+++ P8.cpp
@@ -5,77 +5,99 @@ using namespace std;
 class String {
 private:
     char* str;
-    int length;
 
 public:
-    String() : str(nullptr), length(0) {}
-
-    String(const char* s) {
-        length = strlen(s);
-        str = new char[length + 1];
+    // Constructor
+    String(const char* s = "") {
+        str = new char[strlen(s) + 1];
         strcpy(str, s);
     }
 
-    String(const String& other) {
-        length = other.length;
-        str = new char[length + 1];
-        strcpy(str, other.str);
-    }
-
+    // Destructor
     ~String() {
         delete[] str;
     }
 
+    // Copy constructor
+    String(const String& other) {
+        str = new char[strlen(other.str) + 1];
+        strcpy(str, other.str);
+    }
+
+    // Overload assignment operator
     String& operator=(const String& other) {
         if (this != &other) {
             delete[] str;
-            length = other.length;
-            str = new char[length + 1];
+            str = new char[strlen(other.str) + 1];
             strcpy(str, other.str);
         }
         return *this;
     }
 
+    // Overload + operator for concatenation
     String operator+(const String& other) const {
-        char* temp = new char[length + other.length + 1];
-        strcpy(temp, str);
-        strcat(temp, other.str);
-        return String(temp);
+        String result;
+        delete[] result.str;
+        result.str = new char[strlen(str) + strlen(other.str) + 1];
+        strcpy(result.str, str);
+        strcat(result.str, other.str);
+        return result;
+    }
+
+    // Overload comparison operators
+    bool operator==(const String& other) const {
+        return strcmp(str, other.str) == 0;
     }
 
     bool operator<(const String& other) const {
         return strcmp(str, other.str) < 0;
     }
 
-    bool operator==(const String& other) const {
-        return strcmp(str, other.str) == 0;
-    }
-
+    // Display function
     void display() const {
         cout << str;
     }
 };
 
 int main() {
-    String s1("Hello");
-    String s2("World");
+    // Prompt the user to enter strings
+    cout << "Enter the first string: ";
+    char input1[100];
+    cin.getline(input1, 100);
 
+    cout << "Enter the second string: ";
+    char input2[100];
+    cin.getline(input2, 100);
+
+    // Create String objects from user input
+    String s1(input1);
+    String s2(input2);
+    
+    // Concatenation
     String s3 = s1 + s2;
     cout << "Concatenated string: ";
     s3.display();
     cout << endl;
 
-    String s4 = s1;
+    // Assignment
+    String s4;
+    s4 = s3;
     cout << "Assigned string: ";
     s4.display();
     cout << endl;
 
-    if (s1 < s2)
-        cout << "String 1 is less than String 2" << endl;
-    else if (s1 == s2)
-        cout << "String 1 is equal to String 2" << endl;
-    else
-        cout << "String 1 is greater than String 2" << endl;
+    // Comparison
+    if (s1 == s2) {
+        cout << "Strings are equal." << endl;
+    } else {
+        cout << "Strings are not equal." << endl;
+    }
+
+    if (s1 < s2) {
+        cout << "s1 comes before s2." << endl;
+    } else {
+        cout << "s1 does not come before s2." << endl;
+    }
 
     return 0;
 }

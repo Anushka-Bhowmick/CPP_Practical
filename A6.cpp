c791ce79b14834ff25821fe566ef59744f89b3e8
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2024-05-29 22:10:23 +0530

    A6 modify

--- A6.cpp
+++ A6.cpp
@@ -1,51 +1,53 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
-
-class EmployeeReport {
-private:
-    int employeeID;
-    double totalBonus;
-    double totalOvertime;
-
-public:
-    EmployeeReport() : employeeID(0), totalBonus(0), totalOvertime(0) {}
-
-    EmployeeReport(int id, double bonus, double overtime) 
-        : employeeID(id), totalBonus(bonus), totalOvertime(overtime) {}
-
-    void setPara(int id, double bonus, double overtime) {
-        employeeID = id;
-        totalBonus = bonus;
-        totalOvertime = overtime;
-    }
-
-    void setPara() {
-        employeeID = 0;
+class EmployeeReport{
+    private:
+    int employeeId;
+    double totalBonus, totalOvertime;
+    public:
+    EmployeeReport(){
+        employeeId = 0;
         totalBonus = 0;
         totalOvertime = 0;
     }
-
-    void displayReport() const {
-        cout << "Employee ID: " << employeeID << endl;
-        cout << "Total Bonus: $" << totalBonus << endl;
-        cout << "Total Overtime: " << totalOvertime << " hours" << endl;
+    EmployeeReport(int id, double bonus, double overtime){
+        employeeId = id;
+        totalBonus = bonus;
+        totalOvertime = overtime;
+    }
+    void setPara(int id, double bonus, double overtime){
+        employeeId = id;
+        totalBonus = bonus;
+        totalOvertime = overtime;
+    }
+    void setPara(){
+        employeeId = 0;
+        totalBonus = 0;
+        totalOvertime = 0;
+    }
+    void displayReport(){
+        cout<<"Employee ID: "<<employeeId<<endl;
+        cout<<"Total Bonus: "<<totalBonus<<endl;
+        cout<<"Total Overtime: "<<totalOvertime<<endl;
     }
 };
-
-int main() {
-    EmployeeReport emp1(101, 5000.0, 20.5);
-    EmployeeReport emp2;
-    emp2.setPara(102, 7000.0, 25.0);
-    EmployeeReport emp3;
-    emp3.setPara(103, 6000.0, 22.0);
-    EmployeeReport emp4;
-    emp4.setPara(104, 5500.0, 21.0);
-
-    emp1.displayReport();
-    emp2.displayReport();
-    emp3.displayReport();
-    emp4.displayReport();
-
+int main(){
+    int id;
+    double bonus, overtime;
+    cout<<"Enter the employee ID: "<<endl;
+    cin>>id;
+    cout<<"Enter the total bonus: "<<endl;
+    cin>>bonus;
+    cout<<"Enter the total overtime: "<<endl;
+    cin>>overtime;
+    EmployeeReport e1(id, bonus, overtime);
+    e1.displayReport();
+    EmployeeReport e2;
+    e2.setPara(1, 1000, 10);
+    e2.displayReport();
+    e2.setPara();
+    e2.displayReport();
     return 0;
+
 }
